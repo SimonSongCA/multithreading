7b5d682a830b5e23cb381f2d6a499efdd48f2bfd
Simon Song <simonsongca@gmail.com>
2021-04-17 15:48:34 -0700

    Lok free algorithms with Atomic Integer and Atomic Reference implementations.

--- src/LockFree_AtomicInteger_AtomicReference/Atomic_Reference.java
+++ src/LockFree_AtomicInteger_AtomicReference/Atomic_Reference.java
@@ -1,27 +1,4 @@
-package LockFree_AtomicInteger_AtomicReference;/*
- * MIT License
- *
- * Copyright (c) 2019 Michael Pogrebinsky
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
-
+package LockFree_AtomicInteger_AtomicReference;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -29,10 +6,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.concurrent.locks.LockSupport;
 
-/**
- * Atomic References, Compare And Set, Lock-Free High Performance Data Structure
- * https://www.udemy.com/java-multithreading-concurrency-performance-optimization
- */
+
 public class Atomic_Reference {
     public static void main(String[] args) throws InterruptedException {
         //StandardStack<Integer> stack = new StandardStack<>();
